23d5e7aad26c8112bc8c420d20b542cba3c2992c
mlevr <marilys.levreau@isen.yncrea.fr>
2020-05-15 15:09:32 +0200

    création du menu déroulant

--- app/src/main/java/fr/isen/levreau/smartshirtapp/HomeActivity.kt
+++ app/src/main/java/fr/isen/levreau/smartshirtapp/HomeActivity.kt
@@ -1,21 +1,39 @@
 package fr.isen.levreau.smartshirtapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.LinearLayout
-import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.OrientationHelper.HORIZONTAL
 import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        sport_button.setOnClickListener {
+      /*  sport_button.setOnClickListener {
             val intent = Intent(this, SportActivity::class.java)
             startActivity(intent)
-        }
+        } */
+
+        val choix: ArrayList<String> = ArrayList()
+        choix.add("Nouvelle séance")
+        choix.add("Calibration")
+        choix.add("Réglages")
+        choix.add("Suivi")
+
+        val mImage: ArrayList<Int> = ArrayList()
+        mImage.add(R.drawable.calibration)
+        mImage.add(R.drawable.sport)
+        mImage.add(R.drawable.settings)
+        mImage.add(R.drawable.suivi)
+
+        recyclerView.layoutManager= LinearLayoutManager(this, HORIZONTAL, false)
+        recyclerView.adapter = MenuAdapter(choix, mImage)
     }
+
 }
