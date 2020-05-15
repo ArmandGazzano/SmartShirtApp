7a9d47b682f492ef8f6c9426bc8f823f68ee4552
mlevr <marilys.levreau@isen.yncrea.fr>
2020-05-15 17:01:13 +0200

    Menu déroulant fini

--- app/src/main/java/fr/isen/levreau/smartshirtapp/HomeActivity.kt
+++ app/src/main/java/fr/isen/levreau/smartshirtapp/HomeActivity.kt
@@ -1,6 +1,7 @@
 package fr.isen.levreau.smartshirtapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,15 +11,12 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+    val mItent: ArrayList<Intent> = ArrayList()
+
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-      /*  sport_button.setOnClickListener {
-            val intent = Intent(this, SportActivity::class.java)
-            startActivity(intent)
-        } */
 
         val choix: ArrayList<String> = ArrayList()
         choix.add("Nouvelle séance")
@@ -32,8 +30,16 @@ class HomeActivity : AppCompatActivity() {
         mImage.add(R.drawable.settings)
         mImage.add(R.drawable.suivi)
 
+        mItent.add(Intent(this, SportActivity::class.java))
+        mItent.add(Intent(this, HomeActivity::class.java))
+        mItent.add(Intent(this, MainActivity::class.java))
+        mItent.add(Intent(this, SportActivity::class.java))
+
         recyclerView.layoutManager= LinearLayoutManager(this, HORIZONTAL, false)
-        recyclerView.adapter = MenuAdapter(choix, mImage)
+        recyclerView.adapter = MenuAdapter(choix, mImage, mItent, ::onDeviceClicked)
+    }
+    private fun onDeviceClicked(mItent: Intent) {
+        startActivity(mItent)
     }
 
 }
--- app/src/main/java/fr/isen/levreau/smartshirtapp/MenuAdapter.kt
+++ app/src/main/java/fr/isen/levreau/smartshirtapp/MenuAdapter.kt
@@ -1,5 +1,6 @@
 package fr.isen.levreau.smartshirtapp
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import fr.isen.levreau.smartshirtapp.R.layout.activity_home_cell
 import kotlinx.android.synthetic.main.activity_home_cell.view.*
+import kotlin.reflect.KFunction1
 
-class MenuAdapter(val choix: ArrayList<String>, val mImage: List<Int>) : RecyclerView.Adapter<MenuAdapter.ViewHolder>(){
+class MenuAdapter(
+    val choix: ArrayList<String>,
+    val mImage: List<Int>,
+    val mItent: ArrayList<Intent>,
+    private val imageClickListener: KFunction1<@ParameterName(name = "mItent") Intent, Unit>
+    ): RecyclerView.Adapter<MenuAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(
@@ -22,12 +29,16 @@ class MenuAdapter(val choix: ArrayList<String>, val mImage: List<Int>) : Recycle
         holder.title.text = choix[position] 
         val image_id = mImage[position]
         holder.image.setImageResource(image_id)
-
+        holder.menuCell.setOnClickListener {
+            imageClickListener.invoke(mItent[position])
+        }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.menu_button
         val image: ImageView = itemView.image_button
+        var menuCell = itemView.menucell
+
     }
 
 }
